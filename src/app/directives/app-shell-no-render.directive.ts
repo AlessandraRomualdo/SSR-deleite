c3088d47df90c4b940a5caa0005cb470c38fa625
Alessandra Romualdo <romualdo.alessandra23@gmail.com>
2025-10-08 09:37:46 -0300

    feat: implement directives to render in server s ide loading

--- src/app/directives/app-shell-no-render.directive.ts
+++ src/app/directives/app-shell-no-render.directive.ts
@@ -6,6 +6,9 @@ import { Directive, Inject, OnInit, PLATFORM_ID, TemplateRef, ViewContainerRef }
   standalone: true
 })
 export class AppShellNoRenderDirective implements OnInit {
+  // PLATFORM_ID é um token de injeção que representa a plataforma onde a aplicação está sendo executada (navegador ou servidor).
+  // TemplateRef representa o template associado à diretiva.
+  // ViewContainerRef é um contêiner onde as views podem ser inseridas ou removidas dinamicamente.
   constructor(
     @Inject(PLATFORM_ID) private plataformId: Object,
     private templateRef: TemplateRef<any>,
@@ -14,6 +17,10 @@ export class AppShellNoRenderDirective implements OnInit {
 
 
   ngOnInit(): void {
+    // isPlatformServer verifica se a aplicação está sendo executada no servidor.
+    // Se estiver no servidor, a view é limpa (não renderiza nada).
+    // Se estiver no navegador, a view é criada a partir do template associado.
+    // isPLatformBrowser também poderia ser usado para o mesmo propósito.
     if (isPlatformServer(this.plataformId)) {
       this.viewContainer.clear();
     } else {
